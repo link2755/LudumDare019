cd6f9b025d35c22bbd57f0f08b66e9053e79a42a
MasterProject <dan.inafuku@hotmail.com>
2019-04-27 12:42:08 -0300

    teste 12:42

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    private Vector3 targetPos;
+    public float moveSpeed;
+    private Vector2 deltaPosition;
+    public int shakeDuration;
+    private Vector2 shake;
+    public bool gameActive = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(followTarget.transform.position.x, transform.position.y, transform.position.z);
+
+        if (followTarget != null)
+        {
+            targetPos = new Vector3(followTarget.transform.position.x + shake.x, transform.position.y + shake.y, transform.position.z);
+            //transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            transform.position = targetPos;
+
+        }
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float acceleration;
+    public float superAcceleration;
+    public float jumpSpeed;
+    public Rigidbody2D myRigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Run
+
+        acceleration += superAcceleration * Time.deltaTime;
+        if (acceleration > 5)
+        {
+            acceleration = 5;
+        }
+
+        moveSpeed += acceleration * Time.deltaTime;
+        if (moveSpeed > 18)
+        {
+            moveSpeed = 18;
+        }
+
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        // Jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpSpeed);
+        }
+
+        // Double Jump
+
+        // Scream
+
+        // Slide
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            moveSpeed = -4;
+            acceleration = 2;
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 8);
+
+        }
+    }
+
+}
